47c30771a188c6dc4bd52752a4304a08e51bc6b7
vuthanh199826 <vuthanh199825@gmail.com>
2021-06-08 18:21:25 +0700

    done

---
+++ src/FileCalculatorAdapter.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class FileCalculatorAdapter implements FileCalculator {
+    @Override
+    public long calculateSize(String path) {
+        FileUtil fileUtil = new FileUtil();
+        File file = new File(path);
+        return fileUtil.calculateSize(file);
+    }
+
+}
